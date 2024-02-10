da0c4d0b66bea76064a47812c35f933da0fd4f38
Vitor Nocce <noccevitor@gmail.com>
2024-02-10 16:09:37 -0300

    Exercicio 01

---
+++ Exercicios POO/Exercicio 1/Exercicio 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******** ******** *\r\n " +
+                              "*        *      * *\r\n " +
+                              "*        *      * *\r\n " +
+                              "******** *      * *\r\n " +
+                              "       * *      * *\r\n " +
+                              "       * *      * *\r\n " +
+                              "******** ******** ******** \r\n");
+        }
+    }
+}
