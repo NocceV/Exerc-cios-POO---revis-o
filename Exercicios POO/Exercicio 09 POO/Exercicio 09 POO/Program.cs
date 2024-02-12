678c7e701369f7a14c03644ece7f8afc6e07ccdb
Vitor Nocce <noccevitor@gmail.com>
2024-02-12 10:41:33 -0300

    Exercicios 09 e 10

---
+++ Exercicios POO/Exercicio 09 POO/Exercicio 09 POO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_09_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine(
+                "                    X               X\r\n " +
+                "                  XXX             XXX\r\n " +
+                "                 XXXXX           XXXXX\r\n " +
+                "                XXXXXXX         XXXXXXX\r\n " +
+                "               XXXXXXXXX       XXXXXXXXX\r\n " +
+                "                XXXXXXX         XXXXXXX\r\n " +
+                "                 XXXXX           XXXXX\r\n " +
+                "                  XXX             XXX\r\n " +
+                "                   X               X ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicios POO/Exercicio 10 POO/Exercicio 10 POO/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_10_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool repetidor = true;
+            double totalPagar = 21.40;
+
+            while (repetidor == true)
+            {
+
+                Console.WriteLine("Bem vindo a Vitor Telecom");
+                Console.WriteLine("Por favor informe quantas ligações interurbanas voce fez");
+                int ligacoesExedentes = int.Parse(Console.ReadLine());
+
+                totalPagar += impulsosAMais(ligacoesExedentes);
+
+              
+
+
+                Console.WriteLine("Agora informe quantas ligações para celulares voce fez");
+                int ligacoesCelular = int.Parse(Console.ReadLine());
+
+                totalPagar += ligCelular(ligacoesCelular);
+
+
+                Console.WriteLine("O total do mes que voce devera pagar é de :" + totalPagar);
+                Console.ReadLine();
+
+            }
+        }
+
+        public static double impulsosAMais(int ligacoesExedentes)
+        {
+            double resultado;
+            if (ligacoesExedentes > 90)
+            {
+                resultado = (ligacoesExedentes - 90) * 0.03;
+            }
+            else
+            {
+                resultado = 0;
+            }
+
+
+            return resultado;
+        }
+
+
+        public static double ligCelular(int ligacoesCelular)
+        {
+            double resultado = ligacoesCelular * 0.40;
+
+            return resultado;
+
+        }
+    }
+}
