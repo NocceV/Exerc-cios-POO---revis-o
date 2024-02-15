f3fcebbf453ccce2aff8414c4b71b5e80220fbf9
Vitor Nocce <noccevitor@gmail.com>
2024-02-15 20:49:56 -0300

    Aula caram sobre criacao de classe

---
+++ Exercicios POO/ConsoleApp3/ConsoleApp3/Hora.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Hora
+    {
+        /*
+        public int hora;
+        public int minuto;
+        public int segundo;
+
+
+        public void verificador(Hora horas)
+        {
+            if (horas.segundo >= 60)
+            {
+                horas.segundo = horas.segundo - 60;
+                horas.minuto += 1;
+            }
+            if(horas.minuto>= 60)
+            {
+                horas.minuto=horas.minuto-60;
+                horas.hora += 1;
+            }
+            if(horas.hora >= 24)
+            {
+                horas.hora = 0;
+            }
+            
+
+        }
+
+        public void incrementador(Hora hora,int horas,int minutos,int segundos)
+        {
+            hora.segundo += segundos;
+            hora.minuto += minuto;
+            hora.hora += horas;
+        }
+
+        public void mostarHora(Hora hora)
+        {
+            string mostar = hora.ToString();
+            Console.WriteLine(mostar);
+        }
+
+        */
+
+        public int hora;
+        public int minuto;
+        public int segundo;
+
+        public bool validar()
+        {
+            bool resposta = false;
+            if(hora >=0 && hora <=23  && minuto >=0 && minuto <= 59 && segundo >= 0 && segundo <= 59)
+            {
+                resposta = true;
+            }
+            return resposta;
+        }
+
+        public void ajustar(int horas,int minutos,int segundos)
+        {
+            this.hora = horas;
+            this.minuto = minutos;
+            this.segundo = segundos;
+
+            if (!validar())
+            {
+                this.hora = this.minuto = this.segundo = 0;
+            }
+        }
+
+        public string horaFormatada()
+        {
+            string horaFormatada = hora.ToString("00");
+            string minutagemFormatada = minuto.ToString("00");
+            string segundoFormatado = segundo.ToString("00");
+
+            return horaFormatada + ":" + minutagemFormatada + ":" + segundoFormatado;
+        }
+
+    }
+}
---
+++ Exercicios POO/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            Hora horario1 = new Hora();
+
+            Console.WriteLine("Informe as horas");
+            Console.WriteLine("Pirmeiramente informe as horas");
+            int horas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Agora informe os minutos");
+            int minutos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Por fim informe os segundos");
+            int segundos = int.Parse(Console.ReadLine());
+
+            horario1.hora = horas;
+            horario1.minuto = minutos;
+            horario1.segundo = segundos;
+
+            */
+
+            Hora minhaHora=new Hora();
+
+            minhaHora.ajustar(20, 46, 05);
+
+            Console.WriteLine(minhaHora.horaFormatada());
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
